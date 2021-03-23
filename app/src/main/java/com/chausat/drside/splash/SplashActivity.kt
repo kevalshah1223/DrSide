7a691c3e6034da253d0761278030b964d41401f1
Keval Shah <kevals@hyperlinkinfosystem.net.in>
2021-03-23 19:18:50 +0530

    [+] Created Splash Screen

--- app/src/main/java/com/chausat/drside/splash/SplashActivity.kt
+++ app/src/main/java/com/chausat/drside/splash/SplashActivity.kt
@@ -1,17 +1,20 @@
 package com.chausat.drside.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.chausat.drside.R
+import com.chausat.drside.authentication.AuthMainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        Handler(Looper.myLooper()!!).postDelayed({}, 2000)
-
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this, AuthMainActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
