232a69085a58094688c9c00acf2e933f471a61e0
Keval Shah <kevals@hyperlinkinfosystem.net.in>
2021-03-27 21:44:41 +0530

    [+] updated 1

--- app/src/main/java/com/chausat/drside/splash/SplashActivity.kt
+++ app/src/main/java/com/chausat/drside/splash/SplashActivity.kt
@@ -5,16 +5,27 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.chausat.drside.CommonTag
 import com.chausat.drside.R
 import com.chausat.drside.authentication.AuthMainActivity
+import com.chausat.drside.home.HomeMainActivity
 
-class SplashActivity : AppCompatActivity() {
+class  SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_splash)
         Handler(Looper.myLooper()!!).postDelayed({
-            startActivity(Intent(this, AuthMainActivity::class.java))
-            finish()
+            val pref = getSharedPreferences(CommonTag.SharedPreferences, MODE_PRIVATE)
+            if(pref.getBoolean(CommonTag.IsLogin, false)){
+                startActivity(Intent(this, HomeMainActivity::class.java))
+                finish()
+            }else {
+                startActivity(Intent(this, AuthMainActivity::class.java))
+                finish()
+            }
+
         }, 2000)
     }
 }
